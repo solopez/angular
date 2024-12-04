a8278106e99efad8c1413179000dde8c26043348
ChiaraB96 <chiaraborgatti.96@gmail.com>
2024-12-04 12:52:16 -0300

    Ejercicio 6 con Plus

--- practica-app/src/app/app.component.ts
+++ practica-app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { Ej2Component } from './components/ej2/ej2.component';
 import { Ej3Component } from './components/ej3/ej3.component';
 import { Ej4Component } from './components/ej4/ej4.component';
 import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [Ej2Component, Ej3Component, Ej4Component, HeaderComponent],
+  imports: [Ej2Component, Ej3Component, Ej4Component, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
